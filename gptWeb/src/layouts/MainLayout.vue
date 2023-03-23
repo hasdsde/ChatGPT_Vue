bf284665df482735c4f08a611ec3236245d1989a
hasdsd <1259305706@qq.com>
2023-03-23 23:55:25 +0800

    first commit

--- gptWeb/src/layouts/MainLayout.vue
+++ gptWeb/src/layouts/MainLayout.vue
@@ -3,7 +3,6 @@
     <q-header elevated>
       <q-toolbar>
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
-
         <q-toolbar-title>
           AI聊天系统
         </q-toolbar-title>
@@ -13,12 +12,6 @@
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-
-        <!-- <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        /> -->
         <Aside />
       </q-list>
     </q-drawer>
@@ -26,6 +19,15 @@
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white ">
+      <q-tabs style="height: 5vh;" inline-label v-model="tab" indicator-color="yellow"
+        class="bg-primary text-white no-shadow">
+        <q-route-tab to="/Chat" name="Chat" icon="forum" label="聊天" />
+        <q-route-tab to="/Template" name="Template" icon="category" label="模板" />
+        <q-route-tab to="/User" name="User" icon="person" label="关于" />
+      </q-tabs>
+    </q-footer>
   </q-layout>
 </template>
 
@@ -33,6 +35,7 @@
 import { ref } from 'vue';
 import Aside from 'src/components/Aside.vue';
 import Account from 'src/components/Account.vue';
+const tab = ref()
 
 // 侧栏开关
 const leftDrawerOpen = ref(false)
--- gptWeb/src/pages/Chat.vue
+++ gptWeb/src/pages/Chat.vue
@@ -1,5 +1,26 @@
 <template>
-  聊天
+  <div class="q-pa-md row justify-center">
+    <div style="width: 100%; ">
+      <q-chat-message name=" me" avatar="https://cdn.quasar.dev/img/avatar1.jpg" :text="['hey, how are you?']" sent />
+      <q-chat-message name="Jane" avatar="https://cdn.quasar.dev/img/avatar2.jpg" :text="['doing fine, how r you?']" />
+    </div>
+  </div>
+  <div class="absolute-bottom bg-grey-2 " style="margin-bottom: 5vh;">
+    <div class="q-pa-md">
+      <q-input class="sent" style="width: 75vw" dense outlined v-model="text" />
+      <q-btn class="sent q-ml-sm" color="primary" label="发送" />
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
+import { ref } from 'vue';
+const text = ref('')
+
+
 </script>
+
+<style>
+.sent {
+  display: inline-block;
+}
+</style>
